c45a7c0fa2382f8a85fe466a5131aeb828dfa877
John Witkowski <john.d.witkowski@gmail.com>
2024-01-07 15:15:09 -0800

    Added methods chapter exercises

---
+++ methods.rb
@@ -0,0 +1,20 @@
+# Exercises from Methods chapter
+
+def greeting(name)
+  puts "Hello, " + name + ". How are you doing?"
+end
+
+greeting("John")
+
+def multiply(a, b)
+  a * b
+end
+
+puts multiply(7, 5)
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
