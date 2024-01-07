436791391a5aa5af28234d2f7d9adc7a184b6cd7
John Witkowski <john.d.witkowski@gmail.com>
2024-01-07 14:38:30 -0800

    Adding return.rb from Methods chapter

---
+++ mutate.rb
@@ -0,0 +1,23 @@
+a = [1, 2, 3]
+
+# Example of a method definition that mutates its argument permanently
+def mutate(array)
+  array.pop
+end
+
+# .pop returns and removes trailing elements.
+
+p "Before mutate method: #{a}"
+p mutate(a)
+p "After mutate method: #{a}"
+
+a = [1, 2, 3]
+
+# Example of a method definition that does not mutate the argument
+def no_mutate(array)
+  array.last
+end
+
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
