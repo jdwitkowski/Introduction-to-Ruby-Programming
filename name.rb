589e66872c3f1d6d046b415bfefa0d35c4237942
John Witkowski <john.d.witkowski@gmail.com>
2024-01-07 00:13:26 -0800

    Name.rb exercise from Variables chapter

--- name.rb
+++ name.rb
@@ -3,6 +3,13 @@
 #
 # Description: From the variables chapter exercises
 
-puts "What is your name?"
-name = gets.chomp
-puts "Hello, #{name}!"
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Hello, #{first_name}!"
+puts "What is your last name?"
+last_name = gets.chomp
+puts "#{first_name} #{last_name}"
+
+# 10.times do 
+#   puts name
+# end
