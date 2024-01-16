ca649c5584e74a7f7b8b7262ebd1b42d61bdf316
John Witkowski <john.d.witkowski@gmail.com>
2024-01-15 20:43:05 -0800

    Pushing arrays exercises

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+# Exercise 1
+#
+# arr = [1, 3, 5, 7, 9, 11]
+# number = 3
+#
+# if arr.include?(number)
+#  puts "#{number} is in the array."
+# end
+#
+# Exercide 3
+#
+# arr = [["test", "hello", "world"],["example", "mem"]]
+# puts arr.last.first
